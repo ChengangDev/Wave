66e5c54662b454ba2691922303dc5598ff4b0a93
chengang <chengang2022@gmail.com>
2016-03-23 23:34:50 +0800

    add error handling

--- Wave/src/test.py
+++ Wave/src/test.py
@@ -2,17 +2,27 @@
 import unittest
 import wave as wv
 
-if __name__ == "__main__":
-    unittest.main()
+
 
 class TestWave(unittest.TestCase):
 
     def test_calc_wave_ratio(self):
         cases=[
-            {"pre_prece":10, "open":10.5, "high":11, "low":9}
+            {"pre_price":10, "open":10.5, "high":11, "low":9},
+            {"pre_price":10, "open":7, "high":11, "low":9},
+            {"pre_price":10, "open":12, "high":11, "low":9},
+            {"pre_price":0, "open":10.5, "high":11, "low":9},
         ]
 
         for case in cases:
-            dict = wv.calc_wave_ratio(case["pre_price"], case["open"], case["high"], case["low"])
-            self.assertEqual(dict["min_ratio"], -0.15)
-            self.assertEqual(dict["max_ratio"], 0.05)
+            try:
+                dict = wv.calc_wave_ratio(case["pre_price"], case["open"], case["high"], case["low"])
+                self.assertEqual(dict["min_ratio"], -0.15)
+                self.assertEqual(dict["max_ratio"], 0.05)
+            except ValueError as ve:
+                print(ve.message)
+            except ZeroDivisionError as zde:
+                print(zde.message)
+
+if __name__ == "__main__":
+    unittest.main()
--- Wave/src/wave.py
+++ Wave/src/wave.py
@@ -5,8 +5,14 @@ import tushare as ts
 from array import array
 from numpy import cumsum
 
-
 def calc_wave_ratio(pre_price, open, high, low):
+    if high < open:
+        raise ValueError("high {0} must not less than open {1}".format(high, open))
+    if low > open:
+        raise ValueError("low {0} must not greater than open {1}".format(low, open))
     max_ratio = (high-open)/pre_price
     min_ratio = (low-open)/pre_price
-    return {"max_ration":max_ratio, "min_ratio":min_ratio}
+    return {"max_ratio":max_ratio, "min_ratio":min_ratio}
+
+def count_max_inner(dev, list):
+    return dev
