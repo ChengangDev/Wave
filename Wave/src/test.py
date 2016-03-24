9238997c6cfccfa17485e48e1135ed5a2b243227
chengang <chengang2022@gmail.com>
2016-03-25 00:06:36 +0800

    add calc_

--- Wave/src/test.py
+++ Wave/src/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 import wave as wv
+import pandas as pd
 
 
 
@@ -23,6 +24,21 @@ class TestWave(unittest.TestCase):
                 print(ve.message)
             except ZeroDivisionError as zde:
                 print(zde.message)
+
+
+    def test_calc_ratio_table(self):
+        wave_ratio_df = pd.DataFrame(
+                [
+                    {"max_ratio":0.01, "min_ratio":-0.01},
+                    {"max_ratio":0.011, "min_ratio":-0.011},
+                    {"max_ratio":0.001, "min_ratio":-0.011},
+                    {"max_ratio":0.015, "min_ratio":-0.015},
+                ]
+        )
+
+        df = wv.calc_ratio_table(wave_ratio_df, max_ratio=0.02, min_ratio=-0.02)
+        self.assertEqual(df["0.0000"]["0.0000"], 4)
+        df.to_csv("/home/chengang/Data/test.csv")
 
 if __name__ == "__main__":
     unittest.main()
--- Wave/src/wave.py
+++ Wave/src/wave.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*- 
 from __future__ import print_function
 import numpy as np
-import tushare as ts
-from array import array
+import pandas as pd
 from numpy import cumsum
 
 def calc_wave_ratio(pre_price, open, high, low):
@@ -14,5 +13,30 @@ def calc_wave_ratio(pre_price, open, high, low):
     min_ratio = (low-open)/pre_price
     return {"max_ratio":max_ratio, "min_ratio":min_ratio}
 
-def count_max_inner(dev, list):
-    return dev
+def calc_ratio_table(wave_ratio_df, max_ratio=0.03, min_ratio=-0.03, gap=0.0001):
+    if gap < 0.0001:
+        raise ValueError("gap {0} should not less than 0.0001".format(gap))
+    if max_ratio <= 0:
+        raise ValueError("max_ratio {0} should greater than 0".format(max_ratio))
+    if min_ratio >= 0:
+        raise ValueError("min_ratio {0} should less than 0".format(min_ratio))
+    if gap > max_ratio or gap > -min_ratio:
+        raise ValueError("gap {0} should not greater than the abs of neither max_ratio {1} nor min_ratio {2}"
+                         .format(gap, max_ratio, min_ratio))
+
+    index_float = np.arange(start=0.0, stop=-min_ratio, step=gap)
+    index = ["%.4f" % i for i in index_float]
+    columns_float = np.arange(start=0.0, stop=max_ratio, step=gap)
+    columns = ["%.4f" % i for i in columns_float]
+    table = pd.DataFrame(0, index=index, columns=columns)
+
+    for i, row in wave_ratio_df.iterrows():
+        col_divider = row["max_ratio"]
+        idx_divider = -row["min_ratio"]
+        for idx in index:
+            idx_float = float(idx)
+            for col in columns:
+                if idx_float <= idx_divider and float(col) <= col_divider:
+                    table[idx][col] += 1
+
+    return table
